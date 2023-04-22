c9a4bfa387d34950031acaea57b600c6bfa9d59e
miku1001 <pauloted.22@gmail.com>
2023-04-22 20:22:25 +0800

    Pseudocodes

---
+++ line_write.py
@@ -0,0 +1,10 @@
+# Write a method in python to write multiple line of text contents into a text file mylife.txt.
+
+# open myfile.txt(append)
+# Let user enter line
+# Let user choose whether add more text per line or stop
+# If y
+# Repeat the process
+# If n
+# Stop the program
+# start
