f2f4b213531e6c3d1d59c48840bfe17e34f18099
miku1001 <pauloted.22@gmail.com>
2023-04-22 20:26:04 +0800

    open myfile.txt

--- line_write.py
+++ line_write.py
@@ -1,6 +1,9 @@
 # Write a method in python to write multiple line of text contents into a text file mylife.txt.
 
+def line_write():
 # open myfile.txt(append)
+    with open("myfile.txt", "a") as myfile:
+
 # Let user enter line
 # Let user choose whether add more text per line or stop
 # If y
