adeae04652f9ced008e3d96c3ff05692642caa1f
miku1001 <pauloted.22@gmail.com>
2023-04-22 21:02:47 +0800

    If user type n + Invalid Key

--- line_write.py
+++ line_write.py
@@ -6,16 +6,22 @@ def line_write():
         while True:    
             # Let user enter line
             enter_line = input("Enter line: ")
-            myfile.write(enter_line + "/n")
+            myfile.write(enter_line + "\n")
             # Let user choose whether add more text per line or stop
             choice = input("Are there more lines y/n? ")
             # If y
             if choice == "y":
                 # Repeat the process
                 enter_line = input("Enter line: ")
-                myfile.write(enter_line + "/n")
+                myfile.write(enter_line + "\n")
                 choice = input("Are there more lines y/n? ")
-# If n
-# Stop the program
+            # If n
+            if choice == "n":
+                # Stop the program
+                break
+            else:
+                print("Invalid Key")
+                choice = input("Are there more lines y/n? ")
+
 # start
 line_write()
