939128a47165b854844fd93e6082383734398ce8
miku1001 <pauloted.22@gmail.com>
2023-04-23 17:26:14 +0800

    Major fix on program loop function + header

--- line_write.py
+++ line_write.py
@@ -1,28 +1,34 @@
 # Write a method in python to write multiple line of text contents into a text file mylife.txt.
 
+# create header
+header = "\033[92mText Line Writer\033[0m"
+print(header.center(55, " "))
+print("Programmed by: \033[94mTed Paulo A. Feranil\033[0m")
+print("=" * 50)
 def line_write():
-# open myfile.txt(append)
-    with open("myfile.txt", "a") as myfile:
-        while True:    
-            # Let user enter line
-            enter_line = input("Enter line: ")
-            myfile.write(enter_line + "\n")
-            # Let user choose whether add more text per line or stop
-            choice = input("Are there more lines y/n? ")
-            # If y
-            if choice == "y":
-                # Repeat the process
-                enter_line = input("Enter line: ")
-                myfile.write(enter_line + "\n")
-                choice = input("Are there more lines y/n? ")
-            # If n
-            if choice == "n":
-                # Stop the program
-                break
-            # If invalid key
-            else:
-                print("Invalid Key")
-                choice = input("Are there more lines y/n? ")
+    # open myfile.txt(append)
+    with open("myfile.txt", "a") as myfile:    
+         # Let user enter line
+        enter_line = input("\033[92mEnter line:\033[0m ")
+        myfile.write(enter_line + "\n")
 
 # start
 line_write()
+
+
+# Let user choose whether add more text per line or stop
+while True:
+    choice = input("Are there more lines y/n? ")
+    # If y
+    if choice == "y":
+        # Repeat the process
+        line_write()
+    # If n
+    elif choice == "n":
+        print("Text written succcesfully.\U0001F973 \U0001F973")
+        # Stop the program
+        break
+    # If invalid key
+    else:
+        print("Invalid Key")
+        continue
